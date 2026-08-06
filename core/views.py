9e78633ea1a33907a171541e44d08eec922407d6
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 20:19:11 +0100

    Update views.py
    Save

--- core/views.py
+++ core/views.py
@@ -4,6 +4,23 @@ from services.models import Service
 from team.models import TeamMember
 from testimonials.models import Testimonial
 from core.models import CompanyStat
+from django.http import JsonResponse
+from django.db.models import Q
+from blog.models import BlogPost
+from services.models import Service
+
+def global_search(request):
+    query = request.GET.get('q', '')
+    results = []
+    if query:
+        projects = Project.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))[:5]
+        posts = BlogPost.objects.filter(Q(title__icontains=query) | Q(content__icontains=query), is_published=True)[:5]
+        services = Service.objects.filter(Q(name__icontains=query) | Q(description__icontains=query), is_active=True)[:5]
+        # you can add more models here
+        results = list(projects) + list(posts) + list(services)
+    data = [{'name': str(item), 'url': item.get_absolute_url()} for item in results]
+    return JsonResponse(data, safe=False)
+
 
 class HomeView(TemplateView):
     template_name = 'pages/index.html'
