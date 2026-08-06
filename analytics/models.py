7c8e758992b75706dc735ac08113d8ab5379e7f0
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 20:11:59 +0100

    Update models.py
    Save

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,14 @@
+# analytics/models.py
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class PageVisit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    path = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    session_key = models.CharField(max_length=40, blank=True)
+
+    def __str__(self):
+        return f"{self.path} - {self.timestamp}"
