1a0c59aa7d880fe8e04202b238cc254b08ae2847
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 20:08:36 +0100

    Update views.py
    Save

--- careers/views.py
+++ careers/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# careers/views.py
+from django.views.generic import ListView, DetailView
+from .models import JobOpening
 
-# Create your views here.
+class JobListView(ListView):
+    model = JobOpening
+    template_name = 'careers/list.html'
+    context_object_name = 'jobs'
+    queryset = JobOpening.objects.filter(is_active=True)
+
+class JobDetailView(DetailView):
+    model = JobOpening
+    template_name = 'careers/detail.html'
+    context_object_name = 'job'
