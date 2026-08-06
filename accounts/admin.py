8678c5d388510ec1531cafcc26fdcc3e858842e8
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 20:46:34 +0100

    Update admin.py
    Save

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,31 @@
-# accounts/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 from .models import User
 
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    """Custom admin for User model with extra fields."""
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_email_verified', 'last_activity')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'is_email_verified')
+    search_fields = ('username', 'email', 'first_name', 'last_name', 'phone')
+    ordering = ('-date_joined',)
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'phone', 'date_of_birth',
+                                         'avatar', 'bio', 'company', 'job_title')}),
+        (_('Social links'), {'fields': ('website', 'github', 'linkedin', 'twitter')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined', 'last_activity')}),
+        (_('Verification'), {'fields': ('is_email_verified', 'email_verification_token')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(User, CustomUserAdmin)
