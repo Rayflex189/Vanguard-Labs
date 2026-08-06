d7b127b8323fad41fe30815b287f2226594249c0
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 19:43:41 +0100

    Update admin.py
    Save

--- messaging/admin.py
+++ messaging/admin.py
@@ -1,3 +1,7 @@
+# messaging/admin.py
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'recipient', 'subject', 'sent_at', 'is_read')
