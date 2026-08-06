02bb846a2996383960029c059ec3a667edba96b3
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 19:38:00 +0100

    Update admin.py
    Save

--- projects_management/admin.py
+++ projects_management/admin.py
@@ -1,3 +1,7 @@
+# projects_management/admin.py
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('project', 'title', 'due_date', 'completed')
