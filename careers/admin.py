85dd8397ce8b66cf08b2bd3f23bd2fd0aa8aa3a0
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 20:09:25 +0100

    Update admin.py
    Save

--- careers/admin.py
+++ careers/admin.py
@@ -1,3 +1,8 @@
+# careers/admin.py
 from django.contrib import admin
+from .models import JobOpening
 
-# Register your models here.
+@admin.register(JobOpening)
+class JobOpeningAdmin(admin.ModelAdmin):
+    list_display = ('title', 'department', 'location', 'is_active')
+    prepopulated_fields = {'slug': ('title',)}
