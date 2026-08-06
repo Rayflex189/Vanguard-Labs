e0a173e3ddfe59a822dab4bb60278a448627868c
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 19:49:10 +0100

    Update models.py
    Save

--- events/models.py
+++ events/models.py
@@ -1,3 +1,14 @@
+# events/models.py
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    location = models.CharField(max_length=200, blank=True)
+    is_public = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
