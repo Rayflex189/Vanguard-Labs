2e922f7fc4e5f436bd61841eb844268916742385
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 19:14:06 +0100

    Update admin.py
    Save

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import BlogPost, Tag, BlogCategory
+from markdown import markdown
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_date', 'is_published', 'author')
+    list_filter = ('is_published', 'categories', 'tags')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    fieldsets = (
+        ('Main', {'fields': ('title', 'slug', 'content', 'cover_image', 'author')}),
+        ('Metadata', {'fields': ('categories', 'tags', 'is_published')}),
+    )
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(BlogCategory)
+class BlogCategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
