305f133a2ed55d56cf1b2664a47b25638e42ebe5
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 13:11:23 +0100

    Add TeamMember model with various fields
    Save

--- team/models.py
+++ team/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class TeamMember(models.Model):
+    name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='team/')
+    role = models.CharField(max_length=100)
+    bio = models.TextField()
+    skills = models.CharField(max_length=200, help_text='Comma separated')
+    experience = models.IntegerField(help_text='Years of experience')
+    github = models.URLField(blank=True)
+    linkedin = models.URLField(blank=True)
+    portfolio_url = models.URLField(blank=True)
+    social_media = models.JSONField(default=dict, blank=True)  # e.g. {"twitter": "url"}
+    order = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
