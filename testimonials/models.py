765277b7a99d2755de6c99a533a6ad27663ec051
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 13:13:35 +0100

    Add Testimonial model with fields and validators
    Save

--- testimonials/models.py
+++ testimonials/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Testimonial(models.Model):
+    client_name = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='testimonials/', blank=True)
+    review = models.TextField()
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.client_name} - {self.company}"
