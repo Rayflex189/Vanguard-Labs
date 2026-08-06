d4e3db99349a125d7433217d1fe53b137eab5ade
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 19:31:30 +0100

    Register Proposal model with admin interface
    Save

--- proposals/admin.py
+++ proposals/admin.py
@@ -1,3 +1,7 @@
+# proposals/admin.py
 from django.contrib import admin
+from .models import Proposal
 
-# Register your models here.
+@admin.register(Proposal)
+class ProposalAdmin(admin.ModelAdmin):
+    list_display = ('title', 'client', 'budget', 'status')
