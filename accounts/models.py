ecc215558a80ddc3d8845b3f904c5e454015b08c
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 20:45:21 +0100

    Update models.py
    Save

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,52 @@
-# accounts/models.py
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.utils import timezone
+
+class UserManager(BaseUserManager):
+    """Custom user manager to allow email as username (optional)"""
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, password, **extra_fields)
 
 class User(AbstractUser):
-    phone = models.CharField(max_length=20, blank=True)
-    avatar = models.ImageField(upload_to='avatars/', blank=True)
-    bio = models.TextField(blank=True)
-    # Add any extra fields
+    """
+    Custom User model with additional fields.
+    """
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    company = models.CharField(max_length=100, blank=True, null=True)
+    job_title = models.CharField(max_length=100, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    github = models.URLField(blank=True, null=True)
+    linkedin = models.URLField(blank=True, null=True)
+    twitter = models.URLField(blank=True, null=True)
+    is_email_verified = models.BooleanField(default=False)
+    email_verification_token = models.CharField(max_length=100, blank=True, null=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_activity = models.DateTimeField(auto_now=True)
+
+    # Use the custom manager
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
+
+    def get_full_name(self):
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name}"
+        return self.username
+
+    def get_short_name(self):
+        return self.first_name or self.username
