8530589d6ecb6b7dd225020f948c8b41427887ca
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 13:07:50 +0100

    Update models.py
    Save

--- services/models.py
+++ services/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    icon = models.CharField(max_length=50)  # Font Awesome or Heroicon class
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+    order = models.PositiveSmallIntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
