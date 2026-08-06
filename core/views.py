7fed3efa2d39c2c6753c89f9e3e6cdc2f00c4d87
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 19:06:18 +0100

    Update views.py
    Save

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from portfolio.models import Project
+from services.models import Service
+from team.models import TeamMember
+from testimonials.models import Testimonial
+from core.models import CompanyStat
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'pages/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['featured_projects'] = Project.objects.filter(featured=True)[:6]
+        context['services'] = Service.objects.filter(is_active=True).order_by('order')
+        context['team'] = TeamMember.objects.all().order_by('order')[:8]
+        context['testimonials'] = Testimonial.objects.filter(is_published=True).order_by('?')
+        context['stats'] = CompanyStat.objects.all().order_by('order')
+        return context
