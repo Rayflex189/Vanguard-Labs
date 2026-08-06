453ad9f30a81b4e616bde1fb0d6d960186406884
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 19:21:30 +0100

    Update urls.py
    Save

--- vanguard_labs/urls.py
+++ vanguard_labs/urls.py
@@ -1,22 +1,27 @@
-"""
-URL configuration for vanguard_labs project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),           # home, etc.
+    path('portfolio/', include('portfolio.urls')),
+    path('services/', include('services.urls')),
+    path('team/', include('team.urls')),
+    path('testimonials/', include('testimonials.urls')),
+    path('blog/', include('blog.urls')),
+    path('contact/', include('contact.urls')),
+    path('careers/', include('careers.urls')),
+    path('cms/', include('cms.urls')),
+    path('events/', include('events.urls')),
+    path('knowledgebase/', include('knowledgebase.urls')),
+    path('messages/', include('messaging.urls')),
+    path('notifications/', include('notifications.urls')),
+    path('projects/', include('projects_management.urls')),
+    path('proposals/', include('proposals.urls')),
+    # Add others as needed
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
