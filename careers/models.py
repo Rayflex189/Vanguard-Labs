7b19400631a338429f2ca04f1aeb5afdab3c5be0
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 20:10:17 +0100

    Update models.py
    Save

--- careers/models.py
+++ careers/models.py
@@ -1,3 +1,16 @@
+# careers/models.py
 from django.db import models
 
-# Create your models here.
+class JobOpening(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    department = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    description = models.TextField()
+    requirements = models.TextField()
+    is_active = models.BooleanField(default=True)
+    posted_date = models.DateTimeField(auto_now_add=True)
+    closing_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
