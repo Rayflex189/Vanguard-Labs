d3d8528808a57ea70603d7a959c15d8493474de7
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 19:58:38 +0100

    Create urls.py
    Save

---
+++ cms/urls.py
@@ -0,0 +1,8 @@
+# cms/urls.py
+from django.urls import path
+from .views import PageDetailView
+
+app_name = 'cms'
+urlpatterns = [
+    path('<slug:slug>/', PageDetailView.as_view(), name='page'),
+]
