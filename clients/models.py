e87347469664a37fa98aebbba65bd1dd995d108a
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 20:05:52 +0100

    Update models.py
    Save

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,12 @@
+# clients/models.py
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='clients/logos/', blank=True)
+    website = models.URLField(blank=True)
+    industry = models.CharField(max_length=100, blank=True)
+    is_featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
