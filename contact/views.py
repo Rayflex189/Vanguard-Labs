4b76709365021c45e41328c07ef2f861a12e9e4b
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 19:03:54 +0100

    Update views.py
    Save

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactForm
 
-# Create your views here.
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message has been sent! We\'ll get back to you soon.')
+            return redirect('contact')
+    else:
+        form = ContactForm()
+    return render(request, 'pages/contact.html', {'form': form})
