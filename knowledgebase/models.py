296c541cb8e7a15b66442eb1bcb0642b37a0208c
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-06 19:46:42 +0100

    Update models.py
    Save

--- knowledgebase/models.py
+++ knowledgebase/models.py
@@ -1,3 +1,13 @@
+# knowledgebase/models.py
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    category = models.CharField(max_length=100)
+    is_published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
