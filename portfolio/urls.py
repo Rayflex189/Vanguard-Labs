a09bea3cb40557ba1379f16a4bbde396e47ee7d6
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-07 09:27:49 +0100

    Update urls.py
    Save

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import PortfolioListView, PortfolioDetailView
+from .views import PortfolioListView, PortfolioDetailView, autocomplete_projects
 
 app_name = 'portfolio'
 
 urlpatterns = [
     path('', PortfolioListView.as_view(), name='list'),
     path('category/<slug:category_slug>/', PortfolioListView.as_view(), name='category'),
+    path('search/', PortfolioListView.as_view(), name='search'),
+    path('autocomplete/', autocomplete_projects, name='autocomplete'),
     path('<slug:slug>/', PortfolioDetailView.as_view(), name='detail'),
 ]
