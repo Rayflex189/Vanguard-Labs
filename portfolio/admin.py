28379e1f63a4afbb7dd8b4bfcab6c1eccf5b1c77
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-07 09:25:17 +0100

    Update admin.py
    Save

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,35 +1,59 @@
 from django.contrib import admin
-from .models import Project, Category, Technology, ProjectImage
 from django.utils.html import format_html
+from .models import Category, Technology, Project, ProjectImage
 
 class ProjectImageInline(admin.TabularInline):
     model = ProjectImage
     extra = 1
-
-@admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('title', 'client', 'category', 'featured', 'completion_date')
-    list_filter = ('category', 'featured', 'technologies')
-    search_fields = ('title', 'client', 'description')
-    prepopulated_fields = {'slug': ('title',)}
-    inlines = [ProjectImageInline]
-    fieldsets = (
-        ('Basic Info', {'fields': ('title', 'slug', 'description', 'client', 'category', 'technologies')}),
-        ('Media', {'fields': ('cover_image', 'gallery_images')}),
-        ('Details', {'fields': ('completion_date', 'duration', 'featured')}),
-        ('Links', {'fields': ('live_demo_url', 'github_url')}),
-        ('Case Study Content', {
-            'classes': ('collapse',),
-            'fields': ('problem_statement', 'solution', 'process', 'results', 'lessons_learned', 'testimonial')
-        }),
-        ('Team', {'fields': ('team_members',)}),
-    )
+    fields = ('image', 'caption', 'order')
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
-    list_display = ('name',)
+    list_display = ('name', 'order')
 
 @admin.register(Technology)
 class TechnologyAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
     list_display = ('name',)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'client', 'category', 'completion_date', 'featured', 'is_published')
+    list_filter = ('category', 'featured', 'is_published', 'completion_date', 'technologies')
+    search_fields = ('title', 'client', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        ('Basic', {
+            'fields': ('title', 'slug', 'description', 'client', 'category', 'technologies')
+        }),
+        ('Dates & Duration', {
+            'fields': ('completion_date', 'duration')
+        }),
+        ('Media', {
+            'fields': ('cover_image', 'gallery_images')
+        }),
+        ('Links', {
+            'fields': ('live_demo_url', 'github_url')
+        }),
+        ('Status', {
+            'fields': ('featured', 'is_published')
+        }),
+        ('Team & Testimonial', {
+            'fields': ('team_members', 'testimonial')
+        }),
+        ('Case Study', {
+            'fields': ('problem_statement', 'solution', 'process', 'results', 'lessons_learned'),
+            'classes': ('collapse',)
+        }),
+        ('SEO', {
+            'fields': ('meta_title', 'meta_description'),
+            'classes': ('collapse',)
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
+    inlines = [ProjectImageInline]
