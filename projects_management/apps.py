2a8479ae26c1977f868b389a16ff148298c29ef7
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-07 10:03:42 +0100

    Set default auto field for ProjectsManagementConfig
    Save

--- projects_management/apps.py
+++ projects_management/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
 
-
 class ProjectsManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'projects_management'
+
+    def ready(self):
+        # Import signals to register them
+        import projects_management.signals
