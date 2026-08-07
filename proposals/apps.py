9b99853ecbc1bdc97b3b41e3adad2b2621460d19
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-07 10:51:11 +0100

    Update apps.py
    Save

--- proposals/apps.py
+++ proposals/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
 
-
 class ProposalsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'proposals'
+
+    def ready(self):
+        # Load signals to register handlers
+        import proposals.signals
