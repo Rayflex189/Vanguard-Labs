6a843ac4c873e55d6871022593a84b4297fd5f19
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-07 08:06:02 +0100

    Create urls.py
    Save

---
+++ contact/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import ContactView, ContactSuccessView
+
+app_name = 'contact'
+
+urlpatterns = [
+    path('', ContactView.as_view(), name='form'),
+    path('success/', ContactSuccessView.as_view(), name='success'),
+]
