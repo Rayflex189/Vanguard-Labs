031db8cdf9cc2aaed84b6bb5df40b952ae9f1811
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-07 08:19:05 +0100

    Update urls.py
    Save

--- core/urls.py
+++ core/urls.py
@@ -1,27 +1,18 @@
-# core/urls.py
 from django.urls import path
-from django.http import HttpResponse
-from .views import HomeView, global_search
+from .views import HomeView, global_search, robots_txt, maintenance_view
+from django.conf import settings
 
 app_name = 'core'
 
-def robots_txt(request):
-    lines = [
-        "User-Agent: *",
-        "Disallow: /admin/",
-        "Disallow: /dashboard/",
-        "Allow: /",
-        "Sitemap: https://yourdomain.com/sitemap.xml",
-    ]
-    return HttpResponse("\n".join(lines), content_type="text/plain")
-
 urlpatterns = [
-    # Homepage
     path('', HomeView.as_view(), name='home'),
-
-    # Global search (returns JSON)
     path('search/', global_search, name='global_search'),
-
-    # robots.txt
     path('robots.txt', robots_txt, name='robots_txt'),
 ]
+
+# If maintenance mode is enabled, override the home route with a maintenance page.
+# We can add a conditional check in middleware, but here we add a separate URL.
+if getattr(settings, 'ENABLE_MAINTENANCE_MODE', False):
+    urlpatterns = [
+        path('', maintenance_view, name='maintenance'),
+    ] + urlpatterns
